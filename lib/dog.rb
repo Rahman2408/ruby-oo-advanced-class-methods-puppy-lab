0932dccdaedff67ac348a984e87e1c4dc7425d7b
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-24 10:30:35 -0500

    commit tree 32904bece5b2f0233084664973e11ba517f0bb69

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+
+class  Dog
+    attr_accessor :name, :clear_all, :print_all, :save, :all
+
+    @@all=[]
+    def initialize(name)
+        @name = name
+        @save = save
+        @print_all = print_all
+        @clear_all
+       @all = @@all
+    end
+
+    def self.all
+       @@all
+    end
+
+    def self.clear_all
+        @@all.clear 
+    end
+
+    def self.print_all
+      @@all.each {|dog| puts dog.name}
+    end
+
+    def save
+        @@all<<self
+    end
+
+    def name 
+        @name
+    end
+end
